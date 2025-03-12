684738ea1ec4b5a07b15e187348c42d4bc1ea6eb
joaootaviofm <joaootaviofm@gmail.com>
2025-03-12 18:10:34 -0300

    poppins being added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 function App() {
 
   return (
-    <div className="min-h-screen w-screen bg-black overflow-hidden">
+    <div className="font-poppins min-h-screen w-screen bg-black overflow-hidden">
       <Navbar />
     </div>
   )
