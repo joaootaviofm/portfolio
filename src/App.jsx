28a069aa23f10c90473835b1aff183d04e5f7150
joaootaviofm <joaootaviofm@gmail.com>
2025-03-12 20:19:21 -0300

    adding background image and fixing zindex

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import pattern from "./assets/background-pattern.svg";
 
 function App() {
-
   return (
     <div className="font-poppins min-h-screen w-screen bg-black overflow-hidden">
+      <div
+        className="z-[0] absolute inset-0 bg-cover top-20 md:top-15 opacity-50"
+        style={{ backgroundImage: `url(${pattern})` }}
+      >
+      </div>
       <Navbar />
+      <Hero />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
