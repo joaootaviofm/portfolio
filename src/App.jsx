c2e5353f70fc51316d1d7fed044e61a4177484a4
joaootaviofm <joaootaviofm@gmail.com>
2025-03-12 17:46:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+
+  return (
+    <div className="min-h-screen w-screen bg-black overflow-hidden">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Logo from '../assets/logo.svg'
+
+function Navbar(){
+    return(
+        <>
+            <nav className="">
+                <div className="logo">
+                    <Logo/>
+                </div>
+            </nav>
+        </>
+    )
+
+
+}
+
+export default Navbar;
