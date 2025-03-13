a8845529a390bc0863d4966baf5bd19793e7b4bb
joaootaviofm <joaootaviofm@gmail.com>
2025-03-13 13:40:29 -0300

    added description to each technology

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -8,13 +8,18 @@ import { FaJava } from "react-icons/fa"; // <FaJava />
 import { SiSpringboot } from "react-icons/si"; // <SiSpringboot />
 import { FaGithub } from "react-icons/fa"; // <FaGithub />
 import { FaGitAlt } from "react-icons/fa"; // <FaGitAlt />
+import { useState } from "react";
 
 const techStack = [
   {
     icon: <IoLogoJavascript className="text-yellow-300" />,
     name: "Javascript",
   },
-  { icon: <FaHtml5 className="text-orange-600" />, name: "HTML" },
+  {
+    icon: <FaHtml5 className="text-orange-600" />,
+    name: "HTML",
+    analyze: false,
+  },
   { icon: <FaCss3Alt className="text-blue-400" />, name: "CSS" },
   { icon: <FaReact className="text-blue-500" />, name: "React" },
   { icon: <RiNextjsFill />, name: "NextJS" },
@@ -26,6 +31,43 @@ const techStack = [
 ];
 
 function Technologies() {
+  const [text, setText] = useState("Coloque o mouse em cima da linguagem para ver a descricao.");
+
+  function handleText(index) {
+    switch (index) {
+      case 0:
+        setText("Javascript");
+        break;
+      case 1:
+        setText("HTML");
+        break;
+      case 2:
+        setText("CSS");
+        break;
+      case 3:
+        setText("React");
+        break;
+      case 4:
+        setText("NextJS");
+        break;
+      case 5:
+        setText("Tailwind");
+        break;
+      case 6:
+        setText("Kava");
+        break;
+      case 7:
+        setText("Springboot");
+        break;
+      case 8:
+        setText("Github");
+        break;
+      case 9:
+        setText("Git");
+        break;
+    }
+  }
+
   return (
     <div className="relative text-white font-inter mt-[120px] pb-5">
       <div className="flex flex-col justify-center items-center z-10">
@@ -35,12 +77,20 @@ function Technologies() {
             SAIBA MAIS
           </span>
         </p>
-        <div className="bg-white/10 p-10 rounded-lg backdrop-blur-sm border border-white/20 flex items-center gap-10 mt-[30px]">
-          {techStack.map((tech, index) => (
-            <div className="hover:scale-110 duration-300 text-7xl flex flex-col gap-1 items-center" key={index}>
-              {tech.icon}
-            </div>
-          ))}
+        <div className="flex flex-col bg-white/10 p-10 justify-center items-center rounded-lg backdrop-blur-sm border border-white/20 gap-10 mt-[30px]">
+          <div className="flex gap-[4px] items-center">
+            {techStack.map((tech, index) => (
+              <div
+                onMouseEnter={() => handleText(index)}
+                onMouseLeave={() => setText("Coloque o mouse em cima da linguagem para ver a descricao.")}
+                className="hover:scale-110 duration-300 text-7xl flex gap-1 items-center"
+                key={index}
+              >
+                {tech.icon}
+              </div>
+            ))}
+          </div>
+          <p>{text}</p>
         </div>
       </div>
       <div></div>
