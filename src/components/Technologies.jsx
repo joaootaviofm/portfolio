d66f7ad09e9f8ef5a2f64eafdbea61a78fe5162d
joaootaviofm <joaootaviofm@gmail.com>
2025-03-13 08:45:38 -0300

    added Technologies section

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,11 @@
+function Technologies() {
+  return (
+    <div className="relative text-white font-inter mt-[120px]">
+      <div className="z-10">
+        <h1 className="font-extrabold text-center text-[45px]">TECHNOLOGIES</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Technologies;
