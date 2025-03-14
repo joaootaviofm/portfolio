aaec5e929b524b3f8309384abb4768298bde2886
joaootaviofm <joaootaviofm@gmail.com>
2025-03-14 13:39:44 -0300

    fixing index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import { Link } from "react-scroll"
 
 function Navbar() {
-  const navList = ["Home", "Sobre", "Projetos", "Tecnologias"];
+  const navList = ["Home", "Sobre", "Tecnologias", "Projetos"];
 
   return (
     <nav className="relative w-screen flex justify-between items-center px-20 py-7">
@@ -22,13 +22,12 @@ function Navbar() {
       </div>
       <ul className="z-10 hidden md:flex text-white gap-10 font-semibold">
         {navList.map((item, index) => (
-          <li>
+          <li key={index}>
             <motion.button
               initial={{ opacity: 0, x: -100 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.2 * index }}
               viewport={{ once: true}}
-              key={index}
               className="hover:text-transparent bg-clip-text bg-gradient-to-r from-[#9C83FF] to-[#FF9051] cursor-pointer border-b-[1px] border-transparent hover:border-white duration-500"
             >
               <Link 
