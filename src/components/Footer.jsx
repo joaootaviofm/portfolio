a7a9851b04093df1c72f4a270803f09a401898b0
joaootaviofm <joaootaviofm@gmail.com>
2025-03-14 19:03:59 -0300

    footer react-icons added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
-export default function Footer(){
-    return(
-        <div className="mt-[150px]">
-            <p>salve</p>
-        </div>
-    )
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6"; //<FaLinkedin />
+
+
+export default function Footer() {
+  return (
+    <div className="flex flex-col justify-center items-center relative z-10 mt-[150px] mb-[50px]">
+      <div className="text-white text-2xl flex gap-4">
+        <a href="/"><FaLinkedin /></a>
+        <a href="/"><FaGithub /></a>
+      </div>
+    </div>
+  );
 }
