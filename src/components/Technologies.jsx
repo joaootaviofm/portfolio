1baaaf3bdd028ca48e7554e01bec072c53e3b9fa
joaootaviofm <joaootaviofm@gmail.com>
2025-03-13 21:56:38 -0300

    added motion to tech section

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -10,16 +10,15 @@ import { FaGithub } from "react-icons/fa"; // <FaGithub />
 import { FaGitAlt } from "react-icons/fa"; // <FaGitAlt />
 import { useState } from "react";
 
+// eslint-disable-next-line no-unused-vars
+import { motion } from "motion/react";
+
 const techStack = [
   {
     icon: <IoLogoJavascript className="text-yellow-300" />,
     name: "Javascript",
   },
-  {
-    icon: <FaHtml5 className="text-orange-600" />,
-    name: "HTML",
-    analyze: false,
-  },
+  { icon: <FaHtml5 className="text-orange-600" />, name: "HTML" },
   { icon: <FaCss3Alt className="text-blue-400" />, name: "CSS" },
   { icon: <FaReact className="text-blue-500" />, name: "React" },
   { icon: <RiNextjsFill />, name: "NextJS" },
@@ -31,7 +30,9 @@ const techStack = [
 ];
 
 function Technologies() {
-  const [text, setText] = useState("Coloque o mouse em cima da linguagem para ver a descricao.");
+  const [text, setText] = useState(
+    "Coloque o mouse em cima da linguagem para ver a descricao."
+  );
 
   function handleText(index) {
     switch (index) {
@@ -71,23 +72,44 @@ function Technologies() {
   return (
     <div className="relative text-white font-inter mt-[120px] pb-5">
       <div className="flex flex-col justify-center items-center z-10">
-        <h1 className="font-extrabold text-[45px]">TECNOLOGIAS</h1>
-        <p>
+        <motion.h1
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.25 }}
+          viewport={{ once: true }}
+          className="font-extrabold text-[45px]"
+        >
+          TECNOLOGIAS
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, x: 100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          viewport={{ once: true }}
+        >
           <span className="tracking-[5px] text-[12px] text-transparent bg-clip-text bg-gradient-to-r from-[#9C83FF] to-[#FF9051]">
             SAIBA MAIS
           </span>
-        </p>
+        </motion.p>
         <div className="flex flex-col bg-white/10 p-10 justify-center items-center rounded-lg backdrop-blur-sm border border-white/20 gap-10 mt-[30px]">
           <div className="flex gap-[4px] items-center">
             {techStack.map((tech, index) => (
-              <div
+              <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5, delay: 0.2 * index }}
+                viewport={{ once: true }}
                 onMouseEnter={() => handleText(index)}
-                onMouseLeave={() => setText("Coloque o mouse em cima da linguagem para ver a descricao.")}
+                onMouseLeave={() =>
+                  setText(
+                    "Coloque o mouse em cima da linguagem para ver a descricao."
+                  )
+                }
                 className="hover:scale-110 duration-300 text-7xl flex gap-1 items-center"
                 key={index}
               >
                 {tech.icon}
-              </div>
+              </motion.div>
             ))}
           </div>
           <p>{text}</p>
