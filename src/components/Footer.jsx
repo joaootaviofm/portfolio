77e2f1f974389d9b34854613a863e8ed2d345934
joaootaviofm <joaootaviofm@gmail.com>
2025-03-15 12:22:21 -0300

    fixing footer responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,7 +29,7 @@ export default function Footer() {
         >
           MEUS CONTATOS
         </motion.span>
-        <div className="text-2xl md:text-3xl mt-[30px] flex justify-center items-center gap-5">
+        <div className="text-xl md:text-3xl mt-[30px] flex justify-center items-center gap-5">
           <motion.a
             initial={{ opacity: 0, x: -100 }}
             whileInView={{ opacity: 1, x: 0 }}
