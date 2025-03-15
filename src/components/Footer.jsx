c72b32d9c6c16dcbf21e2879ef53af5a8a2372f4
joaootaviofm <joaootaviofm@gmail.com>
2025-03-15 11:20:51 -0300

    motion to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
+import { motion } from "framer-motion";
 
 export default function Footer() {
   return (
@@ -9,31 +10,59 @@ export default function Footer() {
       className="font-inter flex flex-col justify-center items-center relative z-10 mt-[150px] mb-[50px]"
     >
       <div className="text-white flex items-center  justify-center flex-col">
-        <h1 className="text-[45px] font-extrabold">ENTRE EM CONTATO</h1>
-        <span className="tracking-[5px] text-[12px] text-transparent bg-clip-text bg-gradient-to-r from-[#9C83FF] to-[#FF9051]">
+        <motion.h1
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0 }}
+          viewport={{ once: true }}
+          className="text-[45px] font-extrabold"
+        >
+          ENTRE EM CONTATO
+        </motion.h1>
+        <motion.span
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          viewport={{ once: true }}
+          className="tracking-[5px] text-[12px] text-transparent bg-clip-text bg-gradient-to-r from-[#9C83FF] to-[#FF9051]"
+        >
           MEUS CONTATOS
-        </span>
+        </motion.span>
         <div className="text-3xl mt-[30px] flex justify-center items-center gap-5">
-          <a
+          <motion.a
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0 }}
+            viewport={{ once: true }}
             href="https://www.linkedin.com/in/joaootaviofm10/"
             target="blank"
             className="hover:scale-115 duration-300"
           >
             <FaLinkedin />
-          </a>
-          <a
+          </motion.a>
+          <motion.a
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+            viewport={{ once: true }}
             href="https://github.com/joaootaviofm/"
             target="blank"
             className="hover:scale-115 duration-300"
           >
             <FaGithub />
-          </a>
-          <div className="relative pr-9 pl-5 py-2 flex items-center justify-center bg-white rounded-full border hover:scale-110 duration-400 ">
+          </motion.a>
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 1 }}
+            viewport={{ once: true }}
+            className="relative pr-9 pl-5 py-2 flex items-center justify-center bg-white rounded-full border hover:scale-110 duration-400 "
+          >
             <p className="text-base text-black">joaootaviofm@gmail.com</p>
             <span className="absolute right-1 text-black">
               <MdEmail />
             </span>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
